abde6a2ad0d191c3b9c3f4c7f27b440f197d7248
MintSoup <aregak2005@gmail.com>
2026-06-03 13:57:34 +0400

    changes

--- .config/emacs/ai.el
+++ .config/emacs/ai.el
@@ -4,7 +4,18 @@
   :straight t
   :hook (agent-shell-mode . mixed-pitch-mode)
   :config
-  (setq agent-shell-anthropic-claude-environment
-      (agent-shell-make-environment-variables
-       "CLAUDE_CODE_EXECUTABLE" (executable-find "claude")
-       :inherit-env t)))
+  (setq
+   agent-shell-anthropic-claude-environment
+   (agent-shell-make-environment-variables
+	"CLAUDE_CODE_EXECUTABLE" (executable-find "claude")
+	:inherit-env t)
+
+   agent-shell-mcp-servers
+   '(((name . "emacs")
+	  (command . "socat")
+	  (args . ("-" "UNIX-CONNECT:/home/areg/.config/emacs/emacs-mcp-server.sock"))
+	  (env . ())))))
+
+(use-package mcp-server
+  :straight (:type git :host github :repo "rhblind/emacs-mcp-server"
+				   :files ("*.el" "tools/*.el" "mcp-wrapper.py" "mcp-wrapper.sh")))
